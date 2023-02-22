0f825b735f902a06158c224d79f8cda7ae4bf06e
Sammy <saham@users.noreply.github.com>
2023-02-22 01:01:01 -0500

    Update ViewController.swift
    Signed-off-by: Sammy <saham@users.noreply.github.com>

--- ViewController.swift
+++ ViewController.swift
@@ -4,14 +4,14 @@ import UIKit
 class ViewController: UIViewController {
     // MARK: - Variables
     var Products:[SKProduct] = []
-    var SelectedProd: SKProduct?
+    var SelectedProduct: SKProduct?
 
     // MARK: - IBOutlet
     @IBOutlet weak var tableView: UITableView!
 
     // MARK: - IBAction
     @IBAction func Buy(_ sender: UIButton) {
-        if let productToBuy = SelectedProd {
+        if let productToBuy = SelectedProduct {
             IAPManager.shared.purchase(product: productToBuy)
         }
     }
@@ -24,19 +24,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.Products = IAPManager.shared.products
-        SelectedProd = Products.first
+        SelectedProduct = Products.first
         tableView.delegate = self
         tableView.dataSource = self
         IAPManager.shared.delegate = self
     }
 }
-// MARK: - tableView Delegate and Datasource
+// MARK: - tableView Delegate and DataSource
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return Products.count
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        SelectedProd = Products[indexPath.row]
+        SelectedProduct = Products[indexPath.row]
         tableView.reloadData()
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -44,7 +44,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = product.localizedDescription
         cell.detailTextLabel?.text = product.localizedPrice
-        cell.accessoryType = SelectedProd == product ? .checkmark : .none
+        cell.accessoryType = SelectedProduct == product ? .checkmark : .none
         return cell
     }
 }
@@ -52,6 +52,20 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
 // MARK: - IAPManager Delegate
 extension ViewController: IAPHandlerDelegate {
     func status(transaction: SKPaymentTransaction) {
-        // Implement this
+        // Implement this. You can use your UI based on the trasaction's state
+switch transaction.transactionState {
+        case .restored:
+            print("Transaction restored")
+        case .deferred:
+            print("Transaction deferred")
+        case .purchasing:
+            print("purchasing")
+        case .purchased:
+            print("Transaction purchased")
+        case .failed:
+            print("Transaction faild")
+        default:
+            break
+        }
     }
 }
